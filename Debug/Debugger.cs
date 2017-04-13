9a59757017706a16c54c1567789421381bd4399a
fallen <fengzilijie@gmail.com>
2017-04-13 17:40:50 +0800

    +

---
+++ Debug/Debugger.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Suite
+{
+	public class Debuger  {
+
+		static public bool EnableLog = false;
+
+		static public void INFO()
+		{
+			
+		}
+
+		static public void WARN()
+		{
+
+		}
+
+		static public void ERROR()
+		{
+
+		}
+
+
+
+		// 以下需要重构
+
+		static public void Log(object message)
+		{
+			Log(message,null);
+		}
+		static public void Log(object message, Object context)
+		{
+			if(EnableLog)
+			{
+				Debug.Log(message,context);
+			}
+		}
+		static public void LogError(object message)
+		{
+			LogError(message,null);
+		}
+		static public void LogError(object message, Object context)
+		{
+			if(EnableLog)
+			{
+				Debug.LogError(message,context);
+			}
+		}
+		static public void LogWarning(object message)
+		{
+			LogWarning(message,null);
+		}
+		static public void LogWarning(object message, Object context)
+		{
+			if(EnableLog)
+			{
+				Debug.LogWarning(message,context);
+			}
+		}
+	}
+}
+
